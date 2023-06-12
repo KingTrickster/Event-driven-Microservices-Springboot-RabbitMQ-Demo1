4b7b5ac1e2b3057ca8bbbd61b4cb2e5467a30c09
Alejandro Trejo <alextrejores@gmail.com>
2023-06-12 12:39:26 -0600

    Added consumer

---
+++ stock-service/src/main/java/com/trxjster/stockservice/consumer/OrderConsumer.java
@@ -0,0 +1,20 @@
+package com.trxjster.stockservice.consumer;
+
+import com.trxjster.stockservice.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+    private static Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @RabbitListener(queues = "${rabbitmq.queue.order.name}")
+    public void consume(OrderEvent event){
+        LOGGER.info(String.format("Order event received => %s", event));
+
+
+
+    }
+}
